942ab180a278944ad0f0a2cbeb679bece3f77284
main <main@e9014664-5501-0010-9694-ff39e9067fb4>
2016-06-20 15:53:49 +0000

    リファクタリング
    git-svn-id: file:///C/Users/main/SVN/repository/tutorial/trunk@5 e9014664-5501-0010-9694-ff39e9067fb4

---
+++ svn-turorial/src/main/java/SampleProject/svn_turorial/App.java
@@ -0,0 +1,13 @@
+package SampleProject.svn_turorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
